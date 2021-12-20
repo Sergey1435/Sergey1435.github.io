1c4fe52c94fcd7d5bb8a731b0ea07c26d284d6b2
Sergey1435 <96419622+Sergey1435@users.noreply.github.com>
2021-12-21 00:37:47 +0300

    Add files via upload

---
+++ Genry.js
@@ -0,0 +1,4 @@
+
+document.getElementById("hello_image13").addEventListener("click", function() {
+    document.getElementById("p03").innerHTML = "'Стоит лишь ненадолго впустить тьму в свою душу, и больше она тебя не оставит'";
+    });
